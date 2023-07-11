94bf6938e2204f784085eaad2fbe294fff7ea683
山木 <750154432@qq.com>
2023-07-11 18:08:06 +0800

    [feat] update public-api.ts

--- projects/windowx/src/public-api.ts
+++ projects/windowx/src/public-api.ts
@@ -2,6 +2,14 @@
  * Public API Surface of windowx
  */
 
-export * from './lib/components/ng-windowx/window.service';
 export * from './lib/windowx.component';
 export * from './lib/windowx.module';
+export * from './lib/windowx.service';
+export * from './lib/components/icon/close.icon';
+export * from './lib/components/icon/maximize.icon';
+export * from './lib/components/icon/minimize.icon';
+export * from './lib/components/icon/loading.icon';
+export * from './lib/components/icon/maximized.icon';
+export * from './lib/components/dock/dock.component';
+export * from './lib/directive/outlet.module';
+export * from './lib/directive/string-template-outlet.directive';
