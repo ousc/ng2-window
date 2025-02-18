7168a01c492869101a1975e3b8dd923aa76cc086
Ki2d <2628690470@qq.com>
2025-02-18 09:45:40 +0800

    angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import {Ng2WindowService} from "../../projects/windowx/src/lib/ng2-window.servic
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.less']
+    styleUrls: ['./app.component.less'],
+    standalone: false
 })
 export class AppComponent {
     constructor(private _window: Ng2WindowService) {
